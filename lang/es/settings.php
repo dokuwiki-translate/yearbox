0bd82a4d14b4d3c60e4234ec280da802f969c611
Jorge <jorgelpzdemunain@gmail.com>
2021-11-02 11:50:35 +0100

    translation update

---
+++ lang/es/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Jorge <jorgelpzdemunain@gmail.com>
+ */
+$lang['namestructure_o_default'] = '<root> : anyo-mes : <nombre>-anyo-mes-dia';
+$lang['namestructure_o_YearMonthSeperatedNS'] = '<root> : anyo : mes : <nombre>-anyo-mes-dia';
+$lang['namestructure_o_separatedCompletely'] = '<root> : anyo : mes : dia';
+$lang['namestructure_o_YearNS'] = '<root> : anyo : <nombre>-anyo-mes-dia';
